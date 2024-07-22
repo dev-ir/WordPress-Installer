48fed7ff739572ceeb3d2f7c9bbc818945755d3d
unknown <info@developermen.ir>
2024-07-22 22:46:47 +0330

    First version

---
+++ main.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+plain='\033[0m'
+NC='\033[0m' # No Color
+
+
+install_jq() {
+    if ! command -v jq &> /dev/null; then
+        # Check if the system is using apt package manager
+        if command -v apt-get &> /dev/null; then
+            echo -e "${RED}jq is not installed. Installing...${NC}"
+            sleep 1
+            sudo apt-get update
+            sudo apt-get install -y jq
+        else
+            echo -e "${RED}Error: Unsupported package manager. Please install jq manually.${NC}\n"
+            read -p "Press any key to continue..."
+            exit 1
+        fi
+    fi
+}
+
+necessary_package(){
+    install_jq
+}
+
+
+menu(){
+    
+    clear
+    
+    version='1.0'
+    
+    # Get server IP
+    SERVER_IP=$(hostname -I | awk '{print $1}')
+    
+    # Fetch server country using ip-api.com
+    SERVER_COUNTRY=$(curl -sS "http://ip-api.com/json/$SERVER_IP" | jq -r '.country')
+    
+    # Fetch server isp using ip-api.com
+    SERVER_ISP=$(curl -sS "http://ip-api.com/json/$SERVER_IP" | jq -r '.isp')
+    
+    echo "+------------------------------------------------------------------------------------------------+"
+    echo "| __          __           _ _____                      _____           _        _ _             |"
+    echo "| \ \        / /          | |  __ \                    |_   _|         | |      | | |            |"
+    echo "|  \ \  /\  / /__  _ __ __| | |__) | __ ___  ___ ___     | |  _ __  ___| |_ __ _| | | ___ _ __   |"
+    echo "|   \ \/  \/ / _ \|  __/ _  |  ___/  __/ _ \/ __/ __|    | | |  _ \/ __| __/ _  | | |/ _ \  __|  |"
+    echo "|    \  /\  / (_) | | | (_| | |   | | |  __/\__ \__ \   _| |_| | | \__ \ || (_| | | |  __/ |     |"
+    echo "|     \/  \/ \___/|_|  \__,_|_|   |_|  \___||___/___/  |_____|_| |_|___/\__\__,_|_|_|\___|_|     |"
+    echo "+------------------------------------------------------------------------------------------------+"
+    echo -e "|  TELEGRSM CHANNEL: ${YELLOW}@DVHOST_CLOUD${NC}       |        YOUTUBE CHANNEL: ${YELLOW}@DVHOST_CLOUD${NC}                 |"
+    echo -e "+------------------------------------------------------------------------------------------------+${NC}"
+    echo -e "|${GREEN}Server Country    |${NC} $SERVER_COUNTRY"
+    echo -e "|${GREEN}Server IP         |${NC} $SERVER_IP"
+    echo -e "|${GREEN}Server ISP        |${NC} $SERVER_ISP"
+    echo -e "|${GREEN}WordPress Status  |${NC} Not Installed"
+    echo "+------------------------------------------------------------------------------------------------+"
+    echo -e "|${YELLOW}Please choose an option:${NC}"
+    echo "+------------------------------------------------------------------------------------------------+"
+    echo -e $1
+    echo "+------------------------------------------------------------------------------------------------+"
+    echo -e "\033[0m"
+}
+
+loader(){
+    
+    menu "| 1  - Install WordPress \n| 2  - Unistall \n|  0  - Exit"
+    read -p "Enter option number: " choice
+    
+    case $choice in
+        1)
+            install_wordpress
+        ;;
+        2)
+            unistall_wordpress
+        ;;
+        0)
+            echo -e "${GREEN}Exiting program...${NC}"
+            exit 0
+        ;;
+        *)
+            echo "Not valid"
+        ;;
+    esac
+    
+    
+}
+
+unistall_wordpress() {
+    echo -e "${RED}Removing WordPress...${NC}"
+    
+    sudo systemctl stop apache2
+    
+    sudo rm -rf /srv/www/wordpress
+    
+    mysql -u root -p <<EOF
+DROP DATABASE wordpress;
+DROP USER 'wordpress'@'localhost';
+EOF
+    
+    # Remove any leftover configuration files and directories
+    sudo rm -rf /etc/apache2/sites-available/wordpress.conf
+    sudo a2disconf wordpress.conf
+    
+    # Start the Apache service
+    sudo systemctl start apache2
+    
+    echo "WordPress has been successfully uninstalled."
+}
+
+necessary_package
+loader
---
+++ wp_installer.sh
@@ -0,0 +1,110 @@
+## wordpress installation function
+## https://ubuntu.com/tutorials/install-and-configure-wordpress#6-configure-wordpress-to-connect-to-the-database
+
+install_wordpress(){
+    
+    # Update all package
+    sudo apt update -y
+    sudo apt upgrade -y
+    
+    # Install Dependencies
+    sudo apt install apache2  -y
+    sudo apt install ghostscript  -y
+    sudo libapache2-mod-apt install php  -y
+    sudo mysql-server -y
+    sudo apt install php -y
+    sudo apt install php-bcmath -y
+    sudo apt install php-curl -y
+    sudo apt install php-imagick -y
+    sudo apt install php-intl -y
+    sudo apt install php-json -y
+    sudo apt install php-mbstring -y
+    sudo apt install php-mysql -y
+    sudo apt install php-xml -y
+    sudo apt install php-zip -y
+    
+    # Install WordPress
+    sudo mkdir -p /srv/www
+    sudo chown www-data: /srv/www
+    curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+    
+    # Set Define
+    FILE_PATH="/etc/apache2/sites-available/wordpress.conf"
+    FILE_CONTENT="<VirtualHost *:80>\n\tDocumentRoot /srv/www/wordpress\n\t<Directory /srv/www/wordpress>\n\t\tOptions FollowSymLinks\n\t\tAllowOverride Limit Options FileInfo\n\t\tDirectoryIndex index.php\n\t\tRequire all granted\n\t</Directory>\n\t<Directory /srv/www/wordpress/wp-content>\n\t\tOptions FollowSymLinks\n\t\tRequire all granted\n\t</Directory>\n</VirtualHost>"
+    echo -e $FILE_CONTENT | sudo tee $FILE_PATH > /dev/null
+    sudo a2ensite wordpress.conf
+    
+    #Enable the site with:
+    sudo a2ensite wordpress
+    
+    #Enable URL rewriting with:
+    sudo a2enmod rewrite
+    
+    #Disable the default “It Works” site with:
+    sudo a2dissite 000-default
+    # Reload Apache
+    sudo service apache2 reload
+    sudo systemctl restart apache2
+    
+    # Configure database
+    read -sp "Enter MySQL root password: " mysql_password
+    read -s -p "Enter your WordPress password: " wordpress_password
+    
+    # Run the SQL commands
+    echo "CREATE DATABASE wordpress;" | sudo mysql -u root -p$mysql_password
+    echo "CREATE USER wordpress@localhost IDENTIFIED BY '$wordpress_password';" | sudo mysql -u root -p$mysql_password
+    echo "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost;" | sudo mysql -u root -p$mysql_password
+    echo "FLUSH PRIVILEGES;" | sudo mysql -u root -p$mysql_password
+    echo "quit" | sudo mysql -u root -p$mysql_password
+    
+    # simple copy for wp-config.php
+    sudo -u www-data cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php
+    
+    # Define the database settings
+    DB_NAME=wordpress
+    DB_USER=wordpress
+    DB_PASSWORD="$wordpress_password"
+    DB_HOST="localhost"
+    DB_CHARSET="utf8"
+    DB_COLLATE=""
+    
+    # Get the secret keys
+    SECRET_KEYS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+    
+    # Define the table prefix and debugging mode
+    TABLE_PREFIX="wp_"
+    
+    # Define the file path
+    FILE_PATH="/srv/www/wordpress/wp-config.php"
+    
+    # Create the wp-config.php file
+    echo "<?php
+
+            define( 'DB_NAME', '$DB_NAME' );
+
+            define( 'DB_USER', '$DB_USER' );
+
+            define( 'DB_PASSWORD', '$DB_PASSWORD' );
+
+            define( 'DB_HOST', '$DB_HOST' );
+
+            define( 'DB_CHARSET', '$DB_CHARSET' );
+
+            define( 'DB_COLLATE', '$DB_COLLATE' );
+
+            $SECRET_KEYS
+
+            \$table_prefix = '$TABLE_PREFIX';
+
+            define( 'WP_DEBUG', 'false' );
+
+            if ( ! defined( 'ABSPATH' ) ) {
+                define( 'ABSPATH', __DIR__ . '/' );
+            }
+            require_once ABSPATH . 'wp-settings.php';
+    " > "$FILE_PATH"
+    
+    sleep 2
+    echo -e "Now, you need to open your IP and enter the required information to complete the setup"
+    
+}
